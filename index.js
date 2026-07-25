a5dadc4de0a3817607953c2d2b46413f5c1c2e42
mrramzan301 <ukyt4553@gmail.com>
2026-07-25 20:54:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// This file is needed for Vercel to recognize the project
-// It redirects to the API endpoint
-
 export default function handler(req, res) {
     res.status(200).json({
         message: 'OTP API is running!',
